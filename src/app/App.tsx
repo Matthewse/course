e4ed287697947822b73077ca481f3ae361e86aee
Matthewse <m-semenets@mail.ru>
2023-05-06 19:31:36 +0300

    i18n, theme/lang switcher

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,15 +3,21 @@ import { useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppRouter } from './providers/router';
 import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
+import { Suspense } from 'react';
 
 const App = () => {
-    const { theme, toggleTheme } = useTheme();
+    const { theme } = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Navbar />
-            <AppRouter />
-            <button onClick={toggleTheme}>Кнопка</button>
+            <Suspense fallback="">
+                <Navbar />
+                <div className="content-page">
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
         </div>
     );
 };
